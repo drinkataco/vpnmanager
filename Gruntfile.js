4da89e1bf3c2e403cedf6324bba44ffd6ff431ba
Josh Walwyn <me@joshwalwyn.com>
2016-04-20 21:28:39 +0100

    Grunt bower changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,19 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-bower-task');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        clean : {
-            dist : ['bower_components/*/*', '!bower_components/*/dist']
+        bower: {
+            install: {
+                options: {
+                    cleanup: true
+                }
+            }
         }
     });
 
-    grunt.registerTask('default', ['clean:dist']);
+    grunt.registerTask('default', [ 'bower:install']);
 };
